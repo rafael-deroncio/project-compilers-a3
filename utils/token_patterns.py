5d63b66cf0a7a9da0e99b2b5159909589911044e
rderoncio <rafael.deroncio@animaeducacao.com.br>
2023-12-07 11:49:54 -0300

    identação

--- utils/token_patterns.py
+++ utils/token_patterns.py
@@ -14,17 +14,17 @@ class TokenPatterns(Enum):
             List[Tuple[str, str]]: Lista de tuplas contendo padrões e os respectivos nomes de tokens.
         """
         return [
-            (r'\bpara\b', 'FOR'),                 # Padrão para a palavra-chave 'for'
-            (r'\bno\b', 'IN'),                   # Padrão para a palavra-chave 'in'
-            (r'\bintervalo\b', 'RANGE'),             # Padrão para a palavra-chave 'range'
-            (r'\bescreva\b', 'PRINT'),             # Padrão para a palavra-chave 'print'
+            (r'\bpara\b', 'FOR'),                           # Padrão para a palavra-chave 'for'
+            (r'\bno\b', 'IN'),                              # Padrão para a palavra-chave 'in'
+            (r'\bintervalo\b', 'RANGE'),                    # Padrão para a palavra-chave 'range'
+            (r'\bescreva\b', 'PRINT'),                      # Padrão para a palavra-chave 'print'
             (r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', 'IDENTIFIER'),  # Padrão para identificadores válidos
-            (r'\d+', 'INTEGER'),                 # Padrão para números inteiros
-            (r',', 'COMMA'),                     # Padrão para a vírgula
-            (r"'[^']*'", 'STRING'),              # Padrão para strings entre aspas simples
-            (r'"[^"]*"', 'STRING'),              # Padrão para strings entre aspas duplas
-            (r'\(', 'LPAREN'),                   # Padrão para o parêntese esquerdo
-            (r'\)', 'RPAREN'),                   # Padrão para o parêntese direito
-            (r':', 'COLON'),                     # Padrão para dois pontos
-            (r'[^a-zA-Z0-9_ \t\n,\'"]', 'UNKNOWN'),  # Padrão para caracteres desconhecidos
+            (r'\d+', 'INTEGER'),                            # Padrão para números inteiros
+            (r',', 'COMMA'),                                # Padrão para a vírgula
+            (r"'[^']*'", 'STRING'),                         # Padrão para strings entre aspas simples
+            (r'"[^"]*"', 'STRING'),                         # Padrão para strings entre aspas duplas
+            (r'\(', 'LPAREN'),                              # Padrão para o parêntese esquerdo
+            (r'\)', 'RPAREN'),                              # Padrão para o parêntese direito
+            (r':', 'COLON'),                                # Padrão para dois pontos
+            (r'[^a-zA-Z0-9_ \t\n,\'"]', 'UNKNOWN'),         # Padrão para caracteres desconhecidos
         ]
